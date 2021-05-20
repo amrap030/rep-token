66181f2e7aabfc71bb9832373ac9f08ea7d1d0d5
Kevin Hertwig <hertwig@campus.tu-berlin.de>
2021-05-20 02:57:44 +0200

    initialize project

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1 class="mb-6 text-3xl font-extrabold">Home</h1>
+    <p>{{ name }}</p>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+
+const store = useStore()
+
+const name = computed(() => {
+    return store.state.user.name
+})
+</script>
