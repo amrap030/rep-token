4dd35dc9ee57808ebf0b08dc120ebc1c2ed498c4
Kevin Hertwig <hertwig@campus.tu-berlin.de>
2021-06-05 00:47:02 +0200

    added stocks

--- frontend/src/App.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div
-    class="w-screen h-screen pt-12 bg-gradient-to-t from-pink-600 to-pink-50"
-  >
-    <router-view />
-  </div>
-</template>
--- frontend/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import "./tailwind.css";
-import VWave from "v-wave";
-
-createApp(App).use(store).use(router).use(VWave).mount("#app");
